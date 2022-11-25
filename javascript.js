c4d44f6579214594b6bddb122ad03cedc371f896
Luca Di Matteo <dimatteoluca1995@gmail.com>
2022-11-25 21:02:23 +0100

    Add player selection's controls

--- javascript.js
+++ javascript.js
@@ -9,22 +9,42 @@ function getComputerChoice() {
 }
 
 function computerIsWinner(playerSelection, computerSelection) {
-    return playerSelection == "Rock" && computerSelection == "Paper" ||
+    return (playerSelection == "Rock" && computerSelection == "Paper" ||
             playerSelection == "Paper" && computerSelection == "Scissors" ||
-            playerSelection == "Scissors" && computerSelection == "Rock";
+            playerSelection == "Scissors" && computerSelection == "Rock");
 }
 
 function playerIsWinner(playerSelection, computerSelection) {
-    return playerSelection == "Rock" && computerSelection == "Scissors" ||
+    return (playerSelection == "Rock" && computerSelection == "Scissors" ||
             playerSelection == "Paper" && computerSelection == "Rock" ||
-            playerSelection == "Scissors" && computerSelection == "Paper";
+            playerSelection == "Scissors" && computerSelection == "Paper");
 }
 
-function singleRound(playerSelection, computerSelection) {
-    if (computerIsWinner(playerSelection, computerSelection))
-        return `You lose! ${computerSelection} beats ${playerSelection}`;
-    else if (playerIsWinner(playerSelection, computerSelection))
-        return `You win! ${playerSelection} beats ${computerSelection}`;
+function isValidInput(playerSelection) {
+    let lowerCase = playerSelection.toLowerCase();
+    return (lowerCase == "rock" || lowerCase == "paper" || lowerCase == "scissors");
+}
+
+function formatSelection(selection) {
+    let lowerCase = selection.toLowerCase();
+    if (lowerCase == "rock")
+        return "Rock";
+    else if (lowerCase == "paper")
+        return "Paper";
+    else if (lowerCase == "scissors")
+        return "Scissors";
+}
+
+function playRound(playerSelection, computerSelection) {
+    let formattedPlayerSelection;
+    if (!isValidInput(playerSelection))
+        return `The value '${playerSelection}' is not valid!`;
+    else
+        formattedPlayerSelection = formatSelection(playerSelection);
+    if (computerIsWinner(formattedPlayerSelection, computerSelection))
+        return `You lose! ${computerSelection} beats ${formattedPlayerSelection}`;
+    else if (playerIsWinner(formattedPlayerSelection, computerSelection))
+        return `You win! ${formattedPlayerSelection} beats ${computerSelection}`;
     else
         return "It's a draw!";
 }
